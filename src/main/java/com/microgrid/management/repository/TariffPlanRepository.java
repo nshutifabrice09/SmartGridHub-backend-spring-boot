351593c4186b202b4833ffb89a4bcd97f97ba36d
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 22:22:18 +0200

    final-repo-update

---
+++ src/main/java/com/microgrid/management/repository/TariffPlanRepository.java
@@ -0,0 +1,9 @@
+package com.microgrid.management.repository;
+
+import com.microgrid.management.model.TariffPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TariffPlanRepository extends JpaRepository <TariffPlan, Long> {
+}
