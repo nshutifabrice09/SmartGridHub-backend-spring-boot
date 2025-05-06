79fdd381e84a7e537579b1bd3992a0a3ea248e14
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 17:14:14 +0200

    updates-entities

--- src/main/java/com/microgrid/management/model/Alert.java
+++ src/main/java/com/microgrid/management/model/Alert.java
@@ -1,10 +1,20 @@
 package com.microgrid.management.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "alerts")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Alert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String message;
--- src/main/java/com/microgrid/management/model/BillingRecord.java
+++ src/main/java/com/microgrid/management/model/BillingRecord.java
@@ -1,10 +1,20 @@
 package com.microgrid.management.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "billing_records")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BillingRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDate billingDate;
