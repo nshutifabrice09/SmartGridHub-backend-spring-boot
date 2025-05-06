7b5a9569fe9b2b6cf80dcde7df0075c4415aee87
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 21:57:14 +0200

    repo-updates

---
+++ src/main/java/com/microgrid/management/repository/EnergyForecastRepository.java
@@ -0,0 +1,9 @@
+package com.microgrid.management.repository;
+
+import com.microgrid.management.model.EnergyForecast;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnergyForecastRepository extends JpaRepository <EnergyForecast, Long> {
+}
---
+++ src/main/java/com/microgrid/management/repository/EnergyTransactionRepository.java
@@ -0,0 +1,9 @@
+package com.microgrid.management.repository;
+
+import com.microgrid.management.model.EnergyTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnergyTransactionRepository extends JpaRepository <EnergyTransaction, Long> {
+}
