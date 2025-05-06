8cb5d4bf22704a7524cdfb20e97731985b8419ac
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 21:42:16 +0200

    final-entity-annotation-update

--- src/main/java/com/microgrid/management/model/TariffPlan.java
+++ src/main/java/com/microgrid/management/model/TariffPlan.java
@@ -1,17 +1,24 @@
 package com.microgrid.management.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
-
+@Entity
+@Table(name = "tariff_plans")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TariffPlan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private double ratePerKwh;
     private LocalTime startTime;
     private LocalTime endTime;
-
     @ManyToOne
     private Microgrid microgrid;
 }
