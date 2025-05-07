b85ad850c1f3c2ebb65461d259f6745c5e034c4a
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 14:21:08 +0200

    entities-updates

--- src/main/java/com/microgrid/management/repository/MicrogridRepository.java
+++ src/main/java/com/microgrid/management/repository/MicrogridRepository.java
@@ -2,6 +2,8 @@ package com.microgrid.management.repository;
 
 import com.microgrid.management.model.Microgrid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MicrogridRepository extends JpaRepository <Microgrid, Long> {
 }
