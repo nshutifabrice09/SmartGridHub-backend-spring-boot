2e076860d9618a023c16fb74ab448b0aea4c3b84
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-07 14:15:38 +0200

    secong-service-commit

--- src/main/java/com/microgrid/management/service/AlertService.java
+++ src/main/java/com/microgrid/management/service/AlertService.java
@@ -1,2 +1,13 @@
-package com.microgrid.management.service;public interface AlertService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.Alert;
+
+import java.util.List;
+
+public interface AlertService {
+    List<Alert> getAllAlerts();
+    Alert getAlertById(Long id);
+    Alert saveAlert(Alert alert);
+    Alert updateAlert(Long id, Alert alert);
+    void removeAlertById(Long id);
 }
--- src/main/java/com/microgrid/management/service/BillingRecordService.java
+++ src/main/java/com/microgrid/management/service/BillingRecordService.java
@@ -1,2 +1,14 @@
-package com.microgrid.management.service;public class BillingService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.BillingRecord;
+
+import java.util.List;
+
+public interface BillingRecordService {
+    List<BillingRecord> getAllBillingRecords();
+    BillingRecord getBillingRecordById(Long id);
+    BillingRecord saveBillingRecord(BillingRecord billingRecord);
+    BillingRecord updateBillingRecord(Long id, BillingRecord billingRecord);
+    void removeBillingRecordById(Long id);
+
 }
