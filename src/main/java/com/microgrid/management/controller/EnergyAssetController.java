709a5ce24683d3351f686b0a26861eee45914586
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-08 22:51:59 +0200

    controller-updates

--- src/main/java/com/microgrid/management/controller/EnergyAssetController.java
+++ src/main/java/com/microgrid/management/controller/EnergyAssetController.java
@@ -1,9 +1,18 @@
 package com.microgrid.management.controller;
 
+import com.microgrid.management.service.EnergyAssetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class EnergyAssetController {
+
+    private final EnergyAssetService energyAssetService;
+
+    @Autowired
+    public EnergyAssetController(EnergyAssetService energyAssetService){
+        this.energyAssetService=energyAssetService;
+    }
 }
