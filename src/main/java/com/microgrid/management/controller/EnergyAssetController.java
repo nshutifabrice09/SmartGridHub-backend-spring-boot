c6f913578ed309496e999d0b75454af718802279
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-08 22:50:53 +0200

    controller-updates

--- src/main/java/com/microgrid/management/controller/EnergyAssetController.java
+++ src/main/java/com/microgrid/management/controller/EnergyAssetController.java
@@ -1,2 +1,9 @@
-package com.microgrid.management.controller;public class EnergyAssetController {
+package com.microgrid.management.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class EnergyAssetController {
 }
--- src/main/java/com/microgrid/management/service/EnergyAssetServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/EnergyAssetServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.microgrid.management.service;public class EnergyAssetServiceImplementation {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.EnergyAsset;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnergyAssetServiceImplementation implements EnergyAssetService{
+    @Override
+    public List<EnergyAsset> getAllEnergyAssets() {
+        return null;
+    }
+
+    @Override
+    public EnergyAsset getEnergyAsset(Long id) {
+        return null;
+    }
+
+    @Override
+    public EnergyAsset saveEnergyAsset(EnergyAsset energyAsset) {
+        return null;
+    }
+
+    @Override
+    public EnergyAsset updateEnergyAsset(Long id, EnergyAsset energyAsset) {
+        return null;
+    }
+
+    @Override
+    public void removeEnergyAsset(Long id) {
+
+    }
 }
