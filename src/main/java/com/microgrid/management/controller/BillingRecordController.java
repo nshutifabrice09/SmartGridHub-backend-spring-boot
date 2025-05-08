79cfeb5a9180a4423de9da113b11440df3054ea9
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-08 22:35:47 +0200

    controller-commit

--- src/main/java/com/microgrid/management/controller/BillingRecordController.java
+++ src/main/java/com/microgrid/management/controller/BillingRecordController.java
@@ -3,9 +3,7 @@ package com.microgrid.management.controller;
 import com.microgrid.management.model.BillingRecord;
 import com.microgrid.management.service.BillingRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +18,19 @@ public class BillingRecordController {
         this.billingRecordService = billingRecordService;
     }
 
-    @GetMapping("/billings")
+    @GetMapping("/billingRecords")
     public List<BillingRecord> billingRecordList(){
         return billingRecordService.getAllBillingRecords();
     }
+
+    @GetMapping("/billingRecord/{id}")
+    public BillingRecord getBillingRecordById(@PathVariable ("id") Long id){
+        return billingRecordService.getBillingRecordById(id);
+    }
+
+    @PostMapping("/billingRecord")
+    public BillingRecord saveBillingRecord(@RequestBody BillingRecord billingRecord){
+        return billingRecordService.saveBillingRecord(billingRecord);
+    }
+
 }
