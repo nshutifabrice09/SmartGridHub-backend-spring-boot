4d49e191d52089d57bf38c2b5cf9c753e9d3c107
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-08 23:21:51 +0200

    service-updates

--- src/main/java/com/microgrid/management/exception/EnergyAssetNotFoundException.java
+++ src/main/java/com/microgrid/management/exception/EnergyAssetNotFoundException.java
@@ -1,2 +1,8 @@
-package com.microgrid.management.exception;public class EnergyAssetNotFoundException {
+package com.microgrid.management.exception;
+
+public class EnergyAssetNotFoundException extends RuntimeException{
+    public EnergyAssetNotFoundException(Long id){
+        super
+                ("Couldn't find an EnergyAsset with id "+id);
+    }
 }
