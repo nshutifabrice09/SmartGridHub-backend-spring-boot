9ce4e0f428b5f39cd9261d6e5d79f429fb92dd22
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-08 22:04:02 +0200

    second-controller-commit

--- src/main/java/com/microgrid/management/controller/AlertController.java
+++ src/main/java/com/microgrid/management/controller/AlertController.java
@@ -4,6 +4,7 @@ import com.microgrid.management.model.Alert;
 import com.microgrid.management.service.AlertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -16,8 +17,12 @@ public class AlertController {
         this.alertService = alertService;
     }
 
-    @GetMapping("/alertServices")
+    @GetMapping("/alerts")
     public List<Alert> alertList(){
         return alertService.getAllAlerts();
+    }
+    @GetMapping("/alert/{id}")
+    public Alert getAlertById(@PathVariable ("id") Long id){
+        return alertService.getAlertById(id);
     }
 }
