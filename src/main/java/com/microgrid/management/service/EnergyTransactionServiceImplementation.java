b1e7e60240fb90ecbdf11cfcdab2462618819d0f
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-08 23:25:18 +0200

    service-implementation-updates

--- src/main/java/com/microgrid/management/service/EnergyTransactionServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/EnergyTransactionServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.microgrid.management.service;
 
+import com.microgrid.management.model.EnergyTransaction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnergyTransactionServiceImplementation implements EnergyTransactionService{
+    @Override
+    public List<EnergyTransaction> getAllEnergyTransactions() {
+        return null;
+    }
+
+    @Override
+    public EnergyTransaction getEnergyTransactionById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EnergyTransaction saveEnergyTransaction(EnergyTransaction energyTransaction) {
+        return null;
+    }
+
+    @Override
+    public EnergyTransaction updateEnergyTransaction(Long id, EnergyTransaction energyTransaction) {
+        return null;
+    }
+
+    @Override
+    public void removeEnergyTransactionById(Long id) {
+
+    }
 }
