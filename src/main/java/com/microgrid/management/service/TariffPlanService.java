47f08b8db656079ac0abc0a200908cfe6c8dfe31
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 21:51:22 +0200

    update-service-implementation

--- src/main/java/com/microgrid/management/service/TariffPlanService.java
+++ src/main/java/com/microgrid/management/service/TariffPlanService.java
@@ -1,2 +1,14 @@
-package com.microgrid.management.service;public interface TariffPlanService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.TariffPlan;
+
+import java.util.List;
+
+public interface TariffPlanService {
+    List<TariffPlan> getAllTariffPlans();
+    TariffPlan getTariffPlanById(Long id);
+    TariffPlan saveTariffPlan(TariffPlan tariffPlan);
+    TariffPlan updateTariffPlan(Long id, TariffPlan tariffPlan);
+    void removeTariffPlanById(Long id);
+
 }
---
+++ src/main/java/com/microgrid/management/service/TariffPlanServiceImplementation.java
@@ -0,0 +1,34 @@
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.TariffPlan;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TariffPlanServiceImplementaion implements TariffPlanService {
+    @Override
+    public List<TariffPlan> getAllTariffPlans() {
+        return null;
+    }
+
+    @Override
+    public TariffPlan getTariffPlanById(Long id) {
+        return null;
+    }
+
+    @Override
+    public TariffPlan saveTariffPlan(TariffPlan tariffPlan) {
+        return null;
+    }
+
+    @Override
+    public TariffPlan updateTariffPlan(Long id, TariffPlan tariffPlan) {
+        return null;
+    }
+
+    @Override
+    public void removeTariffPlanById(Long id) {
+
+    }
+}
