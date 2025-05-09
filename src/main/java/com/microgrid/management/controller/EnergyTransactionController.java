d2f52c610b9dcb850f10d3e816009a526de87cc2
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 09:15:03 +0200

    exception-update

--- src/main/java/com/microgrid/management/controller/EnergyTransactionController.java
+++ src/main/java/com/microgrid/management/controller/EnergyTransactionController.java
@@ -1,9 +1,13 @@
 package com.microgrid.management.controller;
 
+import com.microgrid.management.model.EnergyTransaction;
 import com.microgrid.management.service.EnergyTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -13,5 +17,10 @@ public class EnergyTransactionController {
     @Autowired
     public EnergyTransactionController (EnergyTransactionService energyTransactionService){
         this.energyTransactionService=energyTransactionService;
+    }
+
+    @GetMapping("/energyTransactions")
+    public List<EnergyTransaction> energyTransactionList(){
+        return energyTransactionService.getAllEnergyTransactions();
     }
 }
