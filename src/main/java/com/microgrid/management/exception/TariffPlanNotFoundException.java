1e5ad481292a9959a361a8b9327a38516e06fb07
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 13:58:46 +0200

    controller-updates

--- src/main/java/com/microgrid/management/exception/TariffPlanNotFoundException.java
+++ src/main/java/com/microgrid/management/exception/TariffPlanNotFoundException.java
@@ -1,2 +1,8 @@
-package com.microgrid.management.exception;public class TariffPlanNotFoundException {
+package com.microgrid.management.exception;
+
+public class TariffPlanNotFoundException extends RuntimeException{
+    public TariffPlanNotFoundException(Long id){
+        super
+                ("Couldn't find a TariffPan with id "+id);
+    }
 }
