b1622678f3e585ed628b27b450fdf3939896a967
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 14:41:36 +0200

    updates-service-commit

--- src/main/java/com/microgrid/management/service/GridHealthReportService.java
+++ src/main/java/com/microgrid/management/service/GridHealthReportService.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface GridHealthReportService {
     List<GridHealthReport> getAllGridHealthReports();
     GridHealthReport getGridHealthReportById(Long id);
-
+    GridHealthReport saveGridHealthReport(GridHealthReport gridHealthReport);
+    GridHealthReport updateGridHealthReport(Long id, GridHealthReport gridHealthReport);
 }
