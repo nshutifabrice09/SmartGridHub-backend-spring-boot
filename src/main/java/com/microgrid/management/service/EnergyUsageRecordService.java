4d5b9a52a9d5c2c94c5bbc46255baa6f64bfd453
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 14:25:56 +0200

    updates-service-implement-cpmmit

--- src/main/java/com/microgrid/management/service/EnergyUsageRecordService.java
+++ src/main/java/com/microgrid/management/service/EnergyUsageRecordService.java
@@ -1,2 +1,13 @@
-package com.microgrid.management.service;public interface EnergyUsageRecordService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.EnergyUsageRecord;
+
+import java.util.List;
+
+public interface EnergyUsageRecordService {
+    List<EnergyUsageRecord> getAllEnergyUsageRecords();
+    EnergyUsageRecord getEnergyUsageRecordById(Long id);
+    EnergyUsageRecord saveEnergyUsageRecord(EnergyUsageRecord energyUsageRecord);
+    EnergyUsageRecord updateEnergyUsageRecord(Long id, EnergyUsageRecord energyUsageRecord);
+    void removeEnergyUsageRecordById(Long id);
 }
