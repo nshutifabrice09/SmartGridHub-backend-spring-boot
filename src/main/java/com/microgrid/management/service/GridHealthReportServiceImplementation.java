015ce854336f7ccce075b9ab1eb114b1626b0ca0
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 14:48:43 +0200

    updates-exception-updates

--- src/main/java/com/microgrid/management/service/GridHealthReportServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/GridHealthReportServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.microgrid.management.service;
 
+import com.microgrid.management.model.GridHealthReport;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GridHealthReportServiceImplementation implements GridHealthReportService{
+    @Override
+    public List<GridHealthReport> getAllGridHealthReports() {
+        return null;
+    }
+
+    @Override
+    public GridHealthReport getGridHealthReportById(Long id) {
+        return null;
+    }
+
+    @Override
+    public GridHealthReport saveGridHealthReport(GridHealthReport gridHealthReport) {
+        return null;
+    }
+
+    @Override
+    public GridHealthReport updateGridHealthReport(Long id, GridHealthReport gridHealthReport) {
+        return null;
+    }
+
+    @Override
+    public void removeById(Long id) {
+
+    }
 }
