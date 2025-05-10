68ddc5fb48f6c55646b9d8e02294356de6e9da43
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 14:35:51 +0200

    updates-service-commit

--- src/main/java/com/microgrid/management/service/EnergyUsageRecordServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/EnergyUsageRecordServiceImplementation.java
@@ -1,8 +1,35 @@
 package com.microgrid.management.service;
 
+import com.microgrid.management.model.EnergyUsageRecord;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EnergyUsageRecordServiceImplementation implements EnergyUsageRecordService{
-    
+
+    @Override
+    public List<EnergyUsageRecord> getAllEnergyUsageRecords() {
+        return null;
+    }
+
+    @Override
+    public EnergyUsageRecord getEnergyUsageRecordById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EnergyUsageRecord saveEnergyUsageRecord(EnergyUsageRecord energyUsageRecord) {
+        return null;
+    }
+
+    @Override
+    public EnergyUsageRecord updateEnergyUsageRecord(Long id, EnergyUsageRecord energyUsageRecord) {
+        return null;
+    }
+
+    @Override
+    public void removeEnergyUsageRecordById(Long id) {
+
+    }
 }
