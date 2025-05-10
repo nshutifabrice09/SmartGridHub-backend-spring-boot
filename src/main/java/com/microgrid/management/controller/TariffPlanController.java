837347ff33cfb46253afd557353eccade1a23df1
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 14:10:25 +0200

    controller-updates

--- src/main/java/com/microgrid/management/controller/TariffPlanController.java
+++ src/main/java/com/microgrid/management/controller/TariffPlanController.java
@@ -1,2 +1,18 @@
-package com.microgrid.management.controller;public class TariffPlanController {
+package com.microgrid.management.controller;
+
+import com.microgrid.management.service.TariffPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class TariffPlanController {
+
+    private final TariffPlanService tariffPlanService;
+
+    @Autowired
+    public TariffPlanController (TariffPlanService tariffPlanService){
+        this.tariffPlanService = tariffPlanService;
+    }
 }
