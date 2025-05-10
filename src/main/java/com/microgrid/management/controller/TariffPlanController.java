9efb8d388b83e7b7dc014a35eeb8417a97047670
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-10 14:12:05 +0200

    updates

--- src/main/java/com/microgrid/management/controller/TariffPlanController.java
+++ src/main/java/com/microgrid/management/controller/TariffPlanController.java
@@ -1,9 +1,14 @@
 package com.microgrid.management.controller;
 
+import com.microgrid.management.model.TariffPlan;
 import com.microgrid.management.service.TariffPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -15,4 +20,15 @@ public class TariffPlanController {
     public TariffPlanController (TariffPlanService tariffPlanService){
         this.tariffPlanService = tariffPlanService;
     }
+
+    @GetMapping("/tariffPlans")
+    public List<TariffPlan> tariffPlanList(){
+        return tariffPlanService.getAllTariffPlans();
+    }
+
+    @GetMapping("/tariffPlan/{id}")
+    public TariffPlan getTariffPlanById(@PathVariable ("id") Long id){
+        return tariffPlanService.getTariffPlanById(id);
+    }
+
 }
