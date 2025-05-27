ccca3e6098b7b8415de31f5512e004fdc8b8db1c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-27 18:35:54 +0200

    model-update-commit

--- src/main/java/com/microgrid/management/model/Microgrid.java
+++ src/main/java/com/microgrid/management/model/Microgrid.java
@@ -1,5 +1,6 @@
 package com.microgrid.management.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,8 +22,58 @@ public class Microgrid {
     private double totalCapacity;
 
     @OneToMany(mappedBy = "microgrid")
+    @JsonIgnore
     private List<User> users;
 
     @OneToMany(mappedBy = "microgrid")
+    @JsonIgnore
     private List<EnergyAsset> assets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getTotalCapacity() {
+        return totalCapacity;
+    }
+
+    public void setTotalCapacity(double totalCapacity) {
+        this.totalCapacity = totalCapacity;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<EnergyAsset> getAssets() {
+        return assets;
+    }
+
+    public void setAssets(List<EnergyAsset> assets) {
+        this.assets = assets;
+    }
 }
--- src/main/java/com/microgrid/management/model/TariffPlan.java
+++ src/main/java/com/microgrid/management/model/TariffPlan.java
@@ -20,5 +20,54 @@ public class TariffPlan {
     private LocalTime startTime;
     private LocalTime endTime;
     @ManyToOne
+    @JoinColumn(name = "microgrid_id")
     private Microgrid microgrid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRatePerKwh() {
+        return ratePerKwh;
+    }
+
+    public void setRatePerKwh(double ratePerKwh) {
+        this.ratePerKwh = ratePerKwh;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Microgrid getMicrogrid() {
+        return microgrid;
+    }
+
+    public void setMicrogrid(Microgrid microgrid) {
+        this.microgrid = microgrid;
+    }
 }
