22f9ab2d99f8b6cb501df14759a119e569a0171c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-27 18:35:11 +0200

    model-update-commit

--- src/main/java/com/microgrid/management/model/GridHealthReport.java
+++ src/main/java/com/microgrid/management/model/GridHealthReport.java
@@ -23,5 +23,54 @@ public class GridHealthReport {
     private double lossPercentage;
 
     @ManyToOne
+    @JoinColumn(name = "microgrid_id")
     private Microgrid microgrid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getGeneratedAt() {
+        return generatedAt;
+    }
+
+    public void setGeneratedAt(LocalDateTime generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+
+    public double getTotalConsumption() {
+        return totalConsumption;
+    }
+
+    public void setTotalConsumption(double totalConsumption) {
+        this.totalConsumption = totalConsumption;
+    }
+
+    public double getTotalProduction() {
+        return totalProduction;
+    }
+
+    public void setTotalProduction(double totalProduction) {
+        this.totalProduction = totalProduction;
+    }
+
+    public double getLossPercentage() {
+        return lossPercentage;
+    }
+
+    public void setLossPercentage(double lossPercentage) {
+        this.lossPercentage = lossPercentage;
+    }
+
+    public Microgrid getMicrogrid() {
+        return microgrid;
+    }
+
+    public void setMicrogrid(Microgrid microgrid) {
+        this.microgrid = microgrid;
+    }
 }
--- src/main/java/com/microgrid/management/model/MaintenanceRequest.java
+++ src/main/java/com/microgrid/management/model/MaintenanceRequest.java
@@ -23,8 +23,58 @@ public class MaintenanceRequest {
     private LocalDateTime requestDate;
 
     @ManyToOne
+    @JoinColumn(name = "asset_id")
     private EnergyAsset asset;
 
     @ManyToOne
+    @JoinColumn(name = "requestBy_id")
     private User requestedBy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(LocalDateTime requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public EnergyAsset getAsset() {
+        return asset;
+    }
+
+    public void setAsset(EnergyAsset asset) {
+        this.asset = asset;
+    }
+
+    public User getRequestedBy() {
+        return requestedBy;
+    }
+
+    public void setRequestedBy(User requestedBy) {
+        this.requestedBy = requestedBy;
+    }
 }
