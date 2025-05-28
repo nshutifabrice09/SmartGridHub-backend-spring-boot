cccdb23fe629e1e363dc3d238f68c6a67a335422
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-28 21:52:29 +0200

    service-impl-major-update

--- src/main/java/com/microgrid/management/service/EnergyForecastService.java
+++ src/main/java/com/microgrid/management/service/EnergyForecastService.java
@@ -1,2 +1,13 @@
-package com.microgrid.management.service;public interface EnergyForecastService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.EnergyForecast;
+
+import java.util.List;
+
+public interface EnergyForecastService {
+    List<EnergyForecast> getAllEnergyForecasts();
+    EnergyForecast getEnergyForecastById(Long id);
+    EnergyForecast saveEnergyForecast(EnergyForecast energyForecast, Long microgridId);
+    EnergyForecast updateEnergyForecast(EnergyForecast energyForecast, Long id);
+    void removeEnergyForecast(Long id);
 }
--- src/main/java/com/microgrid/management/service/EnergyForecastServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/EnergyForecastServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.microgrid.management.service;public class EnergyForecastServiceImplementation {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.EnergyForecast;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EnergyForecastServiceImplementation implements EnergyForecastService {
+    @Override
+    public List<EnergyForecast> getAllEnergyForecasts() {
+        return null;
+    }
+
+    @Override
+    public EnergyForecast getEnergyForecastById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EnergyForecast saveEnergyForecast(EnergyForecast energyForecast, Long microgridId) {
+        return null;
+    }
+
+    @Override
+    public EnergyForecast updateEnergyForecast(EnergyForecast energyForecast, Long id) {
+        return null;
+    }
+
+    @Override
+    public void removeEnergyForecast(Long id) {
+
+    }
 }
