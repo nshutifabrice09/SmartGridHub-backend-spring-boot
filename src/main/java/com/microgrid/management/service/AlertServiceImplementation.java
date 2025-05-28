052b9492e9a3e03f2a64255600352804de3166d9
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-28 22:26:07 +0200

    service-impl-parameters-update

--- src/main/java/com/microgrid/management/service/AlertServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/AlertServiceImplementation.java
@@ -1,12 +1,25 @@
 package com.microgrid.management.service;
 
 import com.microgrid.management.model.Alert;
+import com.microgrid.management.repository.AlertRepository;
+import com.microgrid.management.repository.MicrogridRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AlertServiceImplementation implements AlertService{
+
+    private final AlertRepository alertRepository;
+    private final MicrogridRepository microgridRepository;
+
+    @Autowired
+    public AlertServiceImplementation(AlertRepository alertRepository, MicrogridRepository microgridRepository) {
+        this.alertRepository = alertRepository;
+        this.microgridRepository = microgridRepository;
+    }
+
     @Override
     public List<Alert> getAllAlerts() {
         return null;
