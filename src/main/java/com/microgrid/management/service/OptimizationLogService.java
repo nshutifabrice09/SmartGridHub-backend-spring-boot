3e46b5d6edb2a81e96bf30642ed7d294e63e0680
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-28 22:20:57 +0200

    service-impl-update

--- src/main/java/com/microgrid/management/service/OptimizationLogService.java
+++ src/main/java/com/microgrid/management/service/OptimizationLogService.java
@@ -1,2 +1,13 @@
-package com.microgrid.management.service;public interface OptimizationLogService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.OptimizationLog;
+
+import java.util.List;
+
+public interface OptimizationLogService {
+    List<OptimizationLog> getAllOptimizationLogs();
+    OptimizationLog getOptimizationLogById(Long id);
+    OptimizationLog saveOptimizationLog(OptimizationLog optimizationLog, Long microgridId);
+    OptimizationLog updateOptimizationLog(OptimizationLog optimizationLog, Long id);
+    void removeOptimizationLog(Long id);
 }
--- src/main/java/com/microgrid/management/service/OptimizationLogServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/OptimizationLogServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.microgrid.management.service;public class OptimizationLogServiceImplementation {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.OptimizationLog;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OptimizationLogServiceImplementation implements OptimizationLogService{
+    @Override
+    public List<OptimizationLog> getAllOptimizationLogs() {
+        return null;
+    }
+
+    @Override
+    public OptimizationLog getOptimizationLogById(Long id) {
+        return null;
+    }
+
+    @Override
+    public OptimizationLog saveOptimizationLog(OptimizationLog optimizationLog, Long microgridId) {
+        return null;
+    }
+
+    @Override
+    public OptimizationLog updateOptimizationLog(OptimizationLog optimizationLog, Long id) {
+        return null;
+    }
+
+    @Override
+    public void removeOptimizationLog(Long id) {
+
+    }
 }
