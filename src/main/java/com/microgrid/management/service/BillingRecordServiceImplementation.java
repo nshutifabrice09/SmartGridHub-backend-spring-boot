40c36f84e04ab846fa16e81df4311b30d591ca59
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-28 22:27:17 +0200

    service-impl-parameters-update

--- src/main/java/com/microgrid/management/service/BillingRecordServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/BillingRecordServiceImplementation.java
@@ -1,12 +1,24 @@
 package com.microgrid.management.service;
 
 import com.microgrid.management.model.BillingRecord;
+import com.microgrid.management.repository.BillingRecordRepository;
+import com.microgrid.management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BillingRecordServiceImplementation implements BillingRecordService{
+    private final BillingRecordRepository billingRecordRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public BillingRecordServiceImplementation(BillingRecordRepository billingRecordRepository, UserRepository userRepository) {
+        this.billingRecordRepository = billingRecordRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<BillingRecord> getAllBillingRecords() {
         return null;
