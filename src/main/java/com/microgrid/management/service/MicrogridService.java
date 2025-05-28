c9a24d594022b8762b75cbab661f1d69884e3999
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-28 22:16:08 +0200

    service-impl-update

--- src/main/java/com/microgrid/management/service/MicrogridService.java
+++ src/main/java/com/microgrid/management/service/MicrogridService.java
@@ -1,2 +1,13 @@
-package com.microgrid.management.service;public interface MicrogridService {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.Microgrid;
+
+import java.util.List;
+
+public interface MicrogridService {
+    List<Microgrid> getAllMicrogrids();
+    Microgrid getMicrogridById(Long id);
+    Microgrid saveMicrogrid(Microgrid microgrid);
+    Microgrid updateMicrogrid(Long id, Microgrid microgrid);
+    void removeMicrogrid(Long id);
 }
--- src/main/java/com/microgrid/management/service/MicrogridServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/MicrogridServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.microgrid.management.service;public class MicrogridServiceImplementation {
+package com.microgrid.management.service;
+
+import com.microgrid.management.model.Microgrid;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MicrogridServiceImplementation implements MicrogridService{
+    @Override
+    public List<Microgrid> getAllMicrogrids() {
+        return null;
+    }
+
+    @Override
+    public Microgrid getMicrogridById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Microgrid saveMicrogrid(Microgrid microgrid) {
+        return null;
+    }
+
+    @Override
+    public Microgrid updateMicrogrid(Long id, Microgrid microgrid) {
+        return null;
+    }
+
+    @Override
+    public void removeMicrogrid(Long id) {
+
+    }
 }
