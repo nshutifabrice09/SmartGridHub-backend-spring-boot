3148e3beee6d4cdac22ef577505d160d114e7a41
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-29 22:51:05 +0200

    service-impl-commit

--- src/main/java/com/microgrid/management/service/EnergyAssetServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/EnergyAssetServiceImplementation.java
@@ -1,12 +1,27 @@
 package com.microgrid.management.service;
 
 import com.microgrid.management.model.EnergyAsset;
+import com.microgrid.management.repository.EnergyAssetRepository;
+import com.microgrid.management.repository.MicrogridRepository;
+import com.microgrid.management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EnergyAssetServiceImplementation implements EnergyAssetService{
+    private final EnergyAssetRepository energyAssetRepository;
+    private final MicrogridRepository microgridRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public EnergyAssetServiceImplementation(EnergyAssetRepository energyAssetRepository, MicrogridRepository microgridRepository, UserRepository userRepository) {
+        this.energyAssetRepository = energyAssetRepository;
+        this.microgridRepository = microgridRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<EnergyAsset> getAllEnergyAssets() {
         return null;
