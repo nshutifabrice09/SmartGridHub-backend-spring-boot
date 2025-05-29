19e8f16083fdb92f78f6d9e996fec5bf8ad1d62b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-05-29 23:13:16 +0200

    service-impl-generate-constructor-parameters-commit

--- src/main/java/com/microgrid/management/service/UserServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/UserServiceImplementation.java
@@ -1,12 +1,21 @@
 package com.microgrid.management.service;
 
 import com.microgrid.management.model.User;
+import com.microgrid.management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImplementation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<User> getAllUsers() {
         return null;
