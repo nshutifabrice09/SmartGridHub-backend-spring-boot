a14b68036ff3365ad95a44bb1be9237f9c844ce2
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-03 21:21:28 +0200

    Class-service-commit

--- src/main/java/com/microgrid/management/controller/UserController.java
+++ src/main/java/com/microgrid/management/controller/UserController.java
@@ -1,2 +1,34 @@
-package com.microgrid.management.controller;public class UserController {
+package com.microgrid.management.controller;
+
+import com.microgrid.management.model.User;
+import com.microgrid.management.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+    private final UserService userService;
+
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/{microgridId}")
+    public User saveUser(@RequestBody User user, @PathVariable ("microgridId") Long microgridId){
+        return userService.saveUser(user, microgridId);
+    }
+    @GetMapping("/users")
+    public List<User>userList(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable ("id") Long id){
+        return userService.getUserById(id);
+    }
 }
