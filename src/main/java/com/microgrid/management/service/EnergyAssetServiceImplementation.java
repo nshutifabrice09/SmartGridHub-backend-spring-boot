929503f87c082de7527faf6c97bd9298553e1108
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-03 22:26:25 +0200

    service-commit

--- src/main/java/com/microgrid/management/service/EnergyAssetServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/EnergyAssetServiceImplementation.java
@@ -1,6 +1,8 @@
 package com.microgrid.management.service;
 
 import com.microgrid.management.model.EnergyAsset;
+import com.microgrid.management.model.Microgrid;
+import com.microgrid.management.model.User;
 import com.microgrid.management.repository.EnergyAssetRepository;
 import com.microgrid.management.repository.MicrogridRepository;
 import com.microgrid.management.repository.UserRepository;
@@ -34,7 +36,11 @@ public class EnergyAssetServiceImplementation implements EnergyAssetService{
 
     @Override
     public EnergyAsset saveEnergyAsset(EnergyAsset energyAsset, Long microgridId, Long ownerId) {
-        return null;
+        Microgrid microgrid = microgridRepository.findMicrogridById(microgridId);
+        User user = userRepository.findUserById(ownerId);
+        energyAsset.setMicrogrid(microgrid);
+        energyAsset.setOwner(user);
+        return energyAssetRepository.save(energyAsset);
     }
 
     @Override
@@ -44,6 +50,6 @@ public class EnergyAssetServiceImplementation implements EnergyAssetService{
 
     @Override
     public void removeEnergyAsset(Long id) {
-
+        energyAssetRepository.deleteById(id);
     }
 }
