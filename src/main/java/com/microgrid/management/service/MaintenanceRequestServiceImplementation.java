a8be6ff3bdf5c8253027df96fbc335612e5fc432
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-04 23:44:37 +0200

    methods-created-commit

--- src/main/java/com/microgrid/management/service/MaintenanceRequestServiceImplementation.java
+++ src/main/java/com/microgrid/management/service/MaintenanceRequestServiceImplementation.java
@@ -1,6 +1,8 @@
 package com.microgrid.management.service;
 
+import com.microgrid.management.model.EnergyAsset;
 import com.microgrid.management.model.MaintenanceRequest;
+import com.microgrid.management.model.User;
 import com.microgrid.management.repository.EnergyAssetRepository;
 import com.microgrid.management.repository.MaintenanceRequestRepository;
 import com.microgrid.management.repository.UserRepository;
@@ -34,16 +36,27 @@ public class MaintenanceRequestServiceImplementation implements MaintenanceReque
 
     @Override
     public MaintenanceRequest saveMaintenanceRequest(MaintenanceRequest maintenanceRequest, Long assetId, Long requestById) {
-        return null;
+        EnergyAsset energyAsset = energyAssetRepository.findEnergyAssetById(assetId);
+        User user = userRepository.findUserById(requestById);
+        maintenanceRequest.setAsset(energyAsset);
+        maintenanceRequest.setRequestedBy(user);
+        return maintenanceRequestRepository.save(maintenanceRequest);
     }
 
     @Override
     public MaintenanceRequest updateMaintenanceRequest(MaintenanceRequest maintenanceRequest, Long id) {
+        MaintenanceRequest existMaintenanceRequest = maintenanceRequestRepository.findMaintenanceRequestById(id);
+        if(existMaintenanceRequest != null){
+            existMaintenanceRequest.setDescription(maintenanceRequest.getDescription());
+            existMaintenanceRequest.setStatus(maintenanceRequest.getStatus());
+            existMaintenanceRequest.setRequestDate(maintenanceRequest.getRequestDate());
+            return maintenanceRequestRepository.save(existMaintenanceRequest);
+        }
         return null;
     }
 
     @Override
     public void removeMaintenanceRequest(Long id) {
-
+        maintenanceRequestRepository.deleteById(id);
     }
 }
