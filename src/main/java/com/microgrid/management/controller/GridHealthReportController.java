59428f2a7fcae8011fe4a39507b1ae2a4078c304
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-05 11:30:27 +0200

    updates-controller-commit-III

--- src/main/java/com/microgrid/management/controller/GridHealthReportController.java
+++ src/main/java/com/microgrid/management/controller/GridHealthReportController.java
@@ -1,9 +1,11 @@
 package com.microgrid.management.controller;
 
+import com.microgrid.management.model.GridHealthReport;
 import com.microgrid.management.service.GridHealthReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -15,4 +17,30 @@ public class GridHealthReportController {
     public GridHealthReportController(GridHealthReportService gridHealthReportService) {
         this.gridHealthReportService = gridHealthReportService;
     }
+
+    @PostMapping("/gridHealthReport/{microgridId}")
+    public GridHealthReport save(@RequestBody GridHealthReport gridHealthReport, @PathVariable ("microgridId") Long microgridId){
+        return gridHealthReportService.saveGridHealthReport(gridHealthReport, microgridId);
+    }
+
+    @GetMapping("/gridHealthReports")
+    public List<GridHealthReport> gridHealthReportList(){
+        return gridHealthReportService.getAllGridHealthReports();
+    }
+
+    @GetMapping("/gridHealthReport/{id}")
+    public GridHealthReport getGridHealthReport(@PathVariable ("id") Long id){
+        return gridHealthReportService.getGridHealthReportById(id);
+    }
+
+    @PutMapping("/update/gridHealthReport/{id}")
+    public GridHealthReport updateGridHealthReport(@PathVariable ("id") Long id, @RequestBody GridHealthReport gridHealthReport){
+        return gridHealthReportService.updateGridHealthReport(id, gridHealthReport);
+    }
+
+    @DeleteMapping("/delete/gridHealthReport/{id}")
+    public void remove(@PathVariable ("id") Long id){
+        gridHealthReportService.removeById(id);
+    }
+
 }
--- src/main/java/com/microgrid/management/controller/MaintenanceRequestController.java
+++ src/main/java/com/microgrid/management/controller/MaintenanceRequestController.java
@@ -1,2 +1,18 @@
-package com.microgrid.management.controller;public class MaintenanceRequestController {
+package com.microgrid.management.controller;
+
+import com.microgrid.management.service.MaintenanceRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class MaintenanceRequestController {
+
+    private final MaintenanceRequestService maintenanceRequestService;
+
+    @Autowired
+    public MaintenanceRequestController(MaintenanceRequestService maintenanceRequestService) {
+        this.maintenanceRequestService = maintenanceRequestService;
+    }
 }
