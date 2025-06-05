f1316848bdf4db54711b8c143af42a5713034464
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-05 11:39:12 +0200

    updates-controller-commit-III

--- src/main/java/com/microgrid/management/controller/MaintenanceRequestController.java
+++ src/main/java/com/microgrid/management/controller/MaintenanceRequestController.java
@@ -1,9 +1,12 @@
 package com.microgrid.management.controller;
 
+import com.microgrid.management.model.MaintenanceRequest;
 import com.microgrid.management.service.MaintenanceRequestService;
+import com.sun.tools.javac.Main;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -14,5 +17,31 @@ public class MaintenanceRequestController {
     @Autowired
     public MaintenanceRequestController(MaintenanceRequestService maintenanceRequestService) {
         this.maintenanceRequestService = maintenanceRequestService;
+    }
+
+    @PostMapping("/maintenanceRequest/{assetId}/{requestById}")
+    public MaintenanceRequest save(@RequestBody MaintenanceRequest maintenanceRequest,
+                                   @PathVariable ("assetId") Long assetId, @PathVariable ("requestById") Long requestById){
+        return maintenanceRequestService.saveMaintenanceRequest(maintenanceRequest, assetId, requestById);
+    }
+
+    @GetMapping("/maintenanceRequests")
+    public List<MaintenanceRequest> maintenanceRequestList(){
+        return maintenanceRequestService.getAllMaintenanceRequests();
+    }
+
+    @GetMapping("/maintenanceRequest/{id}")
+    public MaintenanceRequest getMaintenanceRequest(@PathVariable ("id") Long id){
+        return maintenanceRequestService.getMaintenanceRequestById(id);
+    }
+
+    @PutMapping("/update/maintenanceRequest/{id}")
+    public MaintenanceRequest update(@RequestBody MaintenanceRequest maintenanceRequest, @PathVariable ("id") Long id){
+        return maintenanceRequestService.updateMaintenanceRequest(maintenanceRequest, id);
+    }
+
+    @DeleteMapping("/delete/maintenanceRequest/{id}")
+    public void remove(@PathVariable ("id") Long id){
+        maintenanceRequestService.removeMaintenanceRequest(id);
     }
 }
