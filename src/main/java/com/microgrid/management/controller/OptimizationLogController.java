7c7f9af9339720c15ba657920d22966a3b553ddc
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-05 11:58:39 +0200

    updates-controller-commit-IV

--- src/main/java/com/microgrid/management/controller/OptimizationLogController.java
+++ src/main/java/com/microgrid/management/controller/OptimizationLogController.java
@@ -1,8 +1,10 @@
 package com.microgrid.management.controller;
 
+import com.microgrid.management.model.OptimizationLog;
 import com.microgrid.management.service.OptimizationLogService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -14,4 +16,20 @@ public class OptimizationLogController {
     public OptimizationLogController(OptimizationLogService optimizationLogService) {
         this.optimizationLogService = optimizationLogService;
     }
+
+    @PostMapping("/optimizationLog/{microgridId}")
+    public OptimizationLog save(@RequestBody OptimizationLog optimizationLog, @PathVariable("microgridId") Long microgridId){
+        return optimizationLogService.saveOptimizationLog(optimizationLog, microgridId);
+    }
+
+    @GetMapping("/optimizationLogs")
+    public List<OptimizationLog> optimizationLogList(){
+        return optimizationLogService.getAllOptimizationLogs();
+    }
+
+    @GetMapping("/optimizationLog/{id}")
+    public OptimizationLog getOptimizationLog(@PathVariable ("id") Long id){
+        return optimizationLogService.getOptimizationLogById(id);
+    }
+    
 }
