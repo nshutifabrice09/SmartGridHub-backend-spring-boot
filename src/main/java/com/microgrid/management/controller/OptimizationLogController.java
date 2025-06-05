1a234af8d7c020d8a91c5abdb50c2d5afcdc5686
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-05 11:55:51 +0200

    updates-controller-commit-IV

--- src/main/java/com/microgrid/management/controller/OptimizationLogController.java
+++ src/main/java/com/microgrid/management/controller/OptimizationLogController.java
@@ -1,2 +1,17 @@
-package com.microgrid.management.controller;public class OptimizationLogController {
+package com.microgrid.management.controller;
+
+import com.microgrid.management.service.OptimizationLogService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class OptimizationLogController {
+
+    private final OptimizationLogService optimizationLogService;
+
+
+    public OptimizationLogController(OptimizationLogService optimizationLogService) {
+        this.optimizationLogService = optimizationLogService;
+    }
 }
