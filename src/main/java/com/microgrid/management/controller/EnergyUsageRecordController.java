f7c89493a8103608cfc48c788014fea50acbfa9c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-05 10:04:18 +0200

    updates-controller-commit-III

--- src/main/java/com/microgrid/management/controller/EnergyUsageRecordController.java
+++ src/main/java/com/microgrid/management/controller/EnergyUsageRecordController.java
@@ -1,2 +1,45 @@
-package com.microgrid.management.controller;public class EnergyUsageRecordController {
+package com.microgrid.management.controller;
+
+import com.microgrid.management.model.EnergyUsageRecord;
+import com.microgrid.management.service.EnergyUsageRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class EnergyUsageRecordController {
+
+    private final EnergyUsageRecordService energyUsageRecordService;
+
+    @Autowired
+    public EnergyUsageRecordController(EnergyUsageRecordService energyUsageRecordService) {
+        this.energyUsageRecordService = energyUsageRecordService;
+    }
+
+    @PostMapping("/energyUsage/{userId}")
+    public EnergyUsageRecord save(@RequestBody EnergyUsageRecord energyUsageRecord, @PathVariable ("userId") Long userId){
+        return energyUsageRecordService.saveEnergyUsageRecord(energyUsageRecord, userId);
+    }
+
+    @GetMapping("/energyUsages")
+    public List<EnergyUsageRecord> energyUsageRecordList(){
+        return energyUsageRecordService.getAllEnergyUsageRecords();
+    }
+
+    @GetMapping("/energyUsage/{id}")
+    public EnergyUsageRecord getEnergyUsageRecord(@PathVariable ("id") Long id){
+        return energyUsageRecordService.getEnergyUsageRecordById(id);
+    }
+
+    @PutMapping("/update/energyUsage/{id}")
+    public EnergyUsageRecord update(@PathVariable ("id") Long id, @RequestBody EnergyUsageRecord energyUsageRecord){
+        return energyUsageRecordService.updateEnergyUsageRecord(id, energyUsageRecord);
+    }
+
+    @DeleteMapping("/delete/energyUsage/{id}")
+    public void remove(@PathVariable ("id") Long id){
+        energyUsageRecordService.removeEnergyUsageRecordById(id);
+    }
 }
--- src/main/java/com/microgrid/management/controller/GridHealthReportController.java
+++ src/main/java/com/microgrid/management/controller/GridHealthReportController.java
@@ -1,2 +1,18 @@
-package com.microgrid.management.controller;public class GridHealthReportController {
+package com.microgrid.management.controller;
+
+import com.microgrid.management.service.GridHealthReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class GridHealthReportController {
+
+    private final GridHealthReportService gridHealthReportService;
+
+    @Autowired
+    public GridHealthReportController(GridHealthReportService gridHealthReportService) {
+        this.gridHealthReportService = gridHealthReportService;
+    }
 }
